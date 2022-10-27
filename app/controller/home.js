b868c99d725362908613e8c266774742cafeb0d3
lly <1776321381@qq.com>
2022-10-27 14:58:30 +0800

    feat：测试增删改查

--- app/controller/home.js
+++ app/controller/home.js
@@ -15,11 +15,8 @@ class HomeController extends Controller {
   }
   async user() {
     const { ctx } = this;
-    const { name, content } = await ctx.service.home.user();
-    ctx.body = {
-      name,
-      content,
-    };
+    const result = await ctx.service.home.user();
+    ctx.body = result;
   }
   // post请求
   async add() {
@@ -27,6 +24,66 @@ class HomeController extends Controller {
     const { title } = ctx.request.body;
     // egg内置了bodyParser中间件来对POST请求body解析成object挂载到ctx.request.body上
     ctx.body = { title };
+  }
+
+  async addUser() {
+    const { ctx } = this;
+    const { name } = ctx.request.body;
+    try {
+      const result = await ctx.service.home.addUser(name);
+      console.log('result', result);
+      ctx.body = {
+        code: 200,
+        msg: '添加成功',
+        data: null,
+      };
+    } catch (err) {
+      ctx.body = {
+        code: 500,
+        msg: '添加失败',
+        data: null,
+      };
+    }
+  }
+
+  async editUser() {
+    const { ctx } = this;
+    const { id, name } = ctx.request.body;
+    try {
+      const result = await ctx.service.home.editUser(id, name);
+      console.log('result', result);
+      ctx.body = {
+        code: 200,
+        msg: '修改成功',
+        data: null,
+      };
+    } catch (err) {
+      ctx.body = {
+        code: 500,
+        msg: '修改失败',
+        data: null,
+      };
+    }
+  }
+
+  async deleteUser() {
+    const { ctx } = this;
+    const { id } = ctx.request.body;
+    try {
+      const result = await ctx.service.home.deleteUser(id);
+      console.log('result', result);
+      ctx.body = {
+        code: 200,
+        msg: '删除成功',
+        data: null,
+      };
+    } catch (err) {
+      ctx.body = {
+        code: 500,
+        msg: '删除失败',
+        data: null,
+      };
+    }
   }
 }
 
--- app/service/home.js
+++ app/service/home.js
@@ -4,11 +4,52 @@
 const Service = require('egg').Service;
 
 class HomeService extends Service {
+  // 查询
   async user() {
-    return {
-      name: '周四',
-      content: '疯狂星期四',
-    };
+    const { app } = this;
+    const QUERY_STR = 'id, name';
+    const sql = `select ${QUERY_STR} from list`;
+    try {
+      // mysql的实例对象已经挂载到app对象下，可以通过app.mysql获取到
+      const result = await app.mysql.query(sql);
+      return result;
+    } catch (err) {
+      console.log('获取用户', err);
+      return null;
+    }
+  }
+  // 新增
+  async addUser(name) {
+    const { app } = this;
+    try {
+      const result = await app.mysql.insert('list', { name });
+      return result;
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
+  }
+  // 修改
+  async editUser(id, name) {
+    const { app } = this;
+    try {
+      const result = await app.mysql.update('list', { name }, { where: { id } });
+      return result;
+    } catch (err) {
+      console.log('修改用户', err);
+      return null;
+    }
+  }
+  // 删除
+  async deleteUser(id) {
+    const { app } = this;
+    try {
+      const result = await app.mysql.delete('list', { id });
+      return result;
+    } catch (err) {
+      console.log('删除用户', err);
+      return null;
+    }
   }
 }
 
